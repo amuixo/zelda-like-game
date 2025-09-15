cab2b11865b2031f8cc60b0c2049aad531b01e25
Angela <angela.c.davids@gmail.com>
2025-09-15 22:55:21 +0200

    code n walking ani
    Finished the Walk Tree, added the animations. works good ^^
    
    added the code for the walk tree, took notes in the code. code might change

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -5,18 +5,27 @@ public class Player : MonoBehaviour
     [Tooltip("The speed of the player")]
     [SerializeField]
     private int playerSpeed = 4;
+
     private Rigidbody2D myRigidbody;
+
     private Vector3 movementChange;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator animator; 
+
+    // start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
         myRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        // set the default animation to face down
+            animator.SetFloat("moveX", 0);
+            animator.SetFloat("moveY", 1);
         
     }
 
-    // Update is called once per frame
+    // update is called once per frame
     void Update()
     {
         
@@ -27,14 +36,29 @@ public class Player : MonoBehaviour
 
     private void HandleMovementAndAnimation()
     {
-        // This way the character doesn't continuously move
+        // this way the character doesn't continuously move
         movementChange = Vector3.zero;
 
-        // I can change what horizontal and vertical means in Unity
+        // i can change what horizontal and vertical means in Unity
         movementChange.x = Input.GetAxisRaw("Horizontal");
         movementChange.y = Input.GetAxisRaw("Vertical");
 
-        // Normalized makes it so you can't go faster no matter what direction
+        // normalized makes it so you can't go faster no matter what direction
         myRigidbody.MovePosition(transform.position + movementChange.normalized * playerSpeed * Time.fixedDeltaTime);
+
+    //if there is movement change the vector is no longer zero
+        if(movementChange != Vector3.zero)
+        {
+            animator.SetFloat("moveX", movementChange.x);
+            animator.SetFloat("moveY", movementChange.y);
+
+            //tells the animator that the player is moving
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            //makes it so the engine knows the player is not moving
+            animator.SetBool("isMoving", false);
+        }
     }
 }
